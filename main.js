70d711c075d68df2d8c6e0c7acba2ed1de77d8d9
dimplembagkar <84089522+dimplembagkar@users.noreply.github.com>
2021-08-27 13:46:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://lh3.googleusercontent.com/vrR6LlskhSU9StvsjFExn0VOVxIKhDR7INn_CiXKSIH6hrQKyIqRfoOZMrfv6c7WxeIgZot2ygAb4jdn8G5m5ogNqOhk8eiNi3HP8OGgne3iNZxrrCOJV6ZOiODebl3UcTWApD45hhhbTtCzroU0pA33DcUAKsZsd2KcADzIWBUyGwsBnJFyzSE8UAfZ3iRU9x47nehRg-W_XaCw0mBTE0zaETWyP88dHh2PUCQYH8N1p38-BFjIibRV4XRA4L-FW5aWBXkgVgryqmOzyST69aGqPxNYY-TotYS5lFyKVOOJCHrl8QQ5IohWwPKOkUil3pG0Ey_KDatoqCAfwEHn32J7YKTLqLKXHjWvN3NHs-8EsHmHqz7OJW_9Y5-0HyFyDEXBThkKv-Qucn_YIeg0RSUkiXhAamlSV-jxfqu3c27Mw8e8pgunFAoXmVM7vPzOFYEcZmQiPSD6tMfy1PTPZW8GIuUByopo1DmqwOrYYxuOKzYZdJGVIM3kMwCxr1rdVbTY5q_BWb0aw9wvSO0Z3UrhQ3cgJhH2gDjs9IUNeI_soma8TmMTFLsAwfZtSa6BFBfq1xVYyQHCVK5S8WxvpgS_KNTHNEEW3qOkCBz9EINwcxqRPStoVuIltne4YRIg79ExnYYeuVVSt-Vb8LJDMCmhF_tRCRXcz4WfL_EWDsM-p5A7evzc1FsbeDq5UEw7DbrgQ8ahznTsNHWYJPBOe59xdQ=w390-h510-no?authuser=2", "https://lh3.googleusercontent.com/djblCK3u8j2z6Rrv_A4yu3tgaUFX9H5BEW1-QdGocjf7ezMqiyZIB50j6aghP8ZS5_j5iZah6cCMM_O_SXJhkasDd6TWbKaor7rM1AfQ6i26_eF9ipcDUr662hDbWYoa889yb7TUKe0TgUbkoAFLnYvHzuRzwq2GQ6IYtRurCog_FAb69J9Q921SI46IPJI_o735k2PWI8Kyhvrj2GxtHBAhsUlFK5sIY46EC7jn1SbNCcJ3vSvXeCVQL_R4oq5P57mECRvSo2yX5OMLJSTuCXAgrtXw_AMdKDndWOMvELBFjcfPs05eJTx4cv3J86usc8qZ6i9qvvij500InGiaqKEpMo8TiWHuotRMSGZIQhqVhNu58zPx5SkQv09GHZmSDstoUK5n7LOoD7aNT4rtNojJj1uDhiY6nPj-YMtxvXDqXwUDVh_iGqfaAs9nOJwcfkGApt7QFjEqXQAQQl38QQp1BuaLctVd1--Js3p5c7kMlipIUMP4lviV-wzpmX_vRkPGgGqgRfL3JoNY_l3deloXeDSXFQNl27_ZCLSyUMdnHaUqxnTWJ44B4_V1XyNEluyh7FFX_d8S0BjnxG9BHTmPsqYjSw69tXSYPoo5cudU1skCoKNhwwexrr9Q9lRw64iK0Tg2qU-9wr3S-Xzv4sz67ktiS91oS1eL5G-zxjWlbalivhs3C6dMmOYRyyf_AU6_rEMhbbtwdw33nUEWkXqYWg=w338-h625-no?authuser=2" , "https://lh3.googleusercontent.com/fife/AAWUweXP8bDtjRJq2yqd-ODNtOGk4T6aBMaR3N4csFp1SZPaf7CZv9SSWXz_0Smu39XhM9Emjs2hUbiX-tL2XBcteEbHw4W-d_wSX4yMQm7YIe9I16MzRk7FQW0e-TGFETm1AJ5uv-ewjdIWo9YzFiNo2GWrg_vBa3ExwBGeP3zS9rXBSwvae81GdZ31OZpQ3kaP5YgrRLrRx2mvhpNhIkJOMEgrSTS8y0IS2Gg9cMGXiMyIe3bwHQCUaawmIS3oEH1RQIbi-hlY8IDF94aMj573NNrS4DyXjg2gotmoSH4QwqmLx1kow3qhYmMxuYuhXbaIN07VsEBVSsMDIcU3fMwhatWiPfGLFsx14RTJl6ZtUeUUD_Ylz6gcEObVW9BNp6AQrA26iIN6P5V2UZwjeAlbgi8XTmYzecXPRlWub2A6Q24PYOHvEsY9XTPAcllYvwI56lxD_JuaSrAHrxBQeWj2uH76v_E_kgiJSx_otU3P5JzxvWiy2BVrSRolvZJtXsNnhhs63zVqnx-wtzTg2B7DdySEAsl0vYjItDs7xQGYjR7uMsaW8kD5mwUtx3h0pOeBO4fQvPi_QO9QEBF2SCdiNzaO7_dQu9rvW2EL5TO7mtXbE3Qkj3LIpHYJc9-ehsnM75QSnHy2smUsPgpDAgA9vJz6bK_4sBrgNIKwAoXHcb0344tuLTGqekr31sEqBBeGccW7ivQSif1LJHTw=w564-h625-no?authuser=2"];
+var names = ["Fmaily Book","Milind Lilakant Bagkar", "Madhura Milind Bagkar", "Dimple Milind Bagkar"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 3 
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i];
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
